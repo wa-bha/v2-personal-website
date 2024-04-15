33b20bee789f3d30f422e4f9407ce093ce29f3b6
Bhavit Wadhwa <bhavit.w04@gmail.com>
2024-04-15 22:35:18 +1200

    chore: add vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
 
 import "./globals.css";
 
@@ -24,6 +25,7 @@ export default function RootLayout({
     <html lang="en" className={`${nohemi.variable} ${satoshi.variable}`}>
       <body>
         {children}
+        <Analytics />
         <SpeedInsights />
       </body>
     </html>
