7804183cdfa7d6a6840bb70ca4fba04236fadd1b
Bhavit Wadhwa <bhavit.w04@gmail.com>
2024-04-17 17:56:31 +1200

    feat: add easter egg

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
   return (
     <main>
       <nav id="nav-bar" className="z-10 h-14 fixed top-0 bottom-auto left-0 right-0 shadow-md">
-        <div className="max-w-[88%] m-auto h-full px-5 flex justify-between items-center">
+        <div className="max-w-[90%] m-auto h-full px-5 flex justify-between items-center">
           <span>bhavit</span>
           <div className="flex gap-5">
             <button className="p-2 pb-1.5">
@@ -37,12 +37,20 @@ export default function Home() {
         className="relative py-8 h-svh w-full flex justify-center items-center overflow-hidden"
       >
         <div className="pb-10 flex items-center justify-center flex-col">
-          <h1 className="font-family text-[10vw] md:text-[5.4em]">Bhavit Wadhwa</h1>
+          <h1 className="font-family text-[10vw] md:text-[5.4em]">
+            <a
+              href="https://en.wikipedia.org/wiki/Flat_white"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Bhavit Wadhwa
+            </a>
+          </h1>
 
           <div className="px-10 md:pt-4 md:px-2 md:w-[48rem]">
-            <p className="font-light text-center w-full text-[3.5vw] md:text-[2em] md:leading-tight">
+            <p className="align-baseline font-light text-center w-full text-[3.5vw] md:text-[2em] md:leading-tight">
               experienced software developer based in wellington
-              <FlagNZ className="pr-1 pl-0.5 md:pl-1.5 inline-flex h-[1em] align-text-bottom" />
+              <FlagNZ className="mx-1 inline-flex h-[1em]" />
               skilled in front-end frameworks
             </p>
           </div>
@@ -59,7 +67,7 @@ export default function Home() {
       </header>
 
       <footer className="shadow-inner">
-        <div className="px-5 max-w-[88%] m-auto h-full flex flex-col justify-between gap-8">
+        <div className="px-5 max-w-[90%] m-auto h-full flex flex-col justify-between gap-8">
           <div className="pt-4 flex justify-between items-center">
             <button className="px-1.5 py-0.5">
               <a href="https://v1.bhavit.dev" target="_blank" rel="noopener noreferrer">
@@ -72,7 +80,7 @@ export default function Home() {
           </div>
 
           {/* Wellington bus */}
-          <div className="pl-1 flex justify-center">
+          <div className="pl-2.5 flex justify-center">
             <Image src={bus} width={450} height={500} alt="bus" />
           </div>
         </div>
