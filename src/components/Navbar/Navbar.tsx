89bbb72ea4fda8585b77559c72cb0bcfad283af2
Bhavit Wadhwa <bhavit.w04@gmail.com>
2024-05-28 22:58:21 +1200

    refactor: navbar itself

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,36 @@
+import Link from "next/link";
+
+const navItems = {
+  "/": {
+    name: "home"
+  }
+  //   "/blog": {
+  //     name: "blog"
+  //   }
+};
+const Navbar = () => {
+  return (
+    <nav
+      id="nav-bar"
+      className="z-10 bg-gradient-to-b from-floral-white h-14 fixed top-0 bottom-auto left-0 right-0 shadow-md"
+    >
+      <div className="max-w-[90%] m-auto h-full px-5 flex items-center">
+        <div className="flex flex-row space-x-4 pr-10">
+          {Object.entries(navItems).map(([path, { name }]) => {
+            return (
+              <Link
+                key={path}
+                href={path}
+                className="font-medium slide-in-underline after:bg-tomato flex align-middle"
+              >
+                {name}
+              </Link>
+            );
+          })}
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
