5ce240e036b9488e95dd0fd14295adc93aaedcda
Bhavit Wadhwa <bhavit.w04@gmail.com>
2024-12-01 13:38:37 +1300

    fix: navigation links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import { MadeInWellingtonBadge } from "@/components/icons";
 export default function Home() {
   return (
     <main>
-      <header className="relative mb-10 h-svh w-full overflow-hidden md:h-[100svh]">
+      <header id="#" className="relative mb-10 h-svh w-full overflow-hidden md:h-[100svh]">
         <HeroSection />
         <div className="absolute bottom-6 right-6 m-2">
           <MadeInWellingtonBadge className="w-28 drop-shadow-md md:w-32 lg:w-36" />
@@ -14,14 +14,14 @@ export default function Home() {
 
       <section className="m-auto my-10 max-w-[90%] px-1 sm:px-5 md:mb-24 2xl:flex 2xl:justify-center">
         <div className="w-full max-w-6xl 2xl:max-w-2xl">
-          <h2>Experience</h2>
+          <h2 id="experience">Experience</h2>
           <ExperienceSection />
         </div>
       </section>
 
       <section className="m-auto my-10 max-w-[90%] px-1 sm:px-5 md:my-24 2xl:flex 2xl:justify-center">
         <div className="max-w-2xl">
-          <h2>Bio</h2>
+          <h2 id="bio">Bio</h2>
           <BioSection />
         </div>
       </section>
