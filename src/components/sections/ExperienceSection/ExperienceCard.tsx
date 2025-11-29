da6159191efe135b68abf908cc3f51f8e523906a
Bhavit Wadhwa <bhavit.w04@gmail.com>
2025-11-29 20:19:10 +1300

    refactor: remove cva library and refactor experience card

--- src/components/sections/ExperienceSection/ExperienceCard.tsx
+++ src/components/sections/ExperienceSection/ExperienceCard.tsx
@@ -2,7 +2,6 @@
 
 import React from "react";
 import Image, { StaticImageData } from "next/image";
-import { cva } from "class-variance-authority";
 import { CircleArrowRight } from "lucide-react";
 
 import ExternalLink from "../../common/ExternalLink";
@@ -15,77 +14,59 @@ import jarden from "/public/images/jarden.jpg";
 import hatch from "/public/images/hatch.jpg";
 import hortplus from "/public/images/hortplus.jpg";
 
-type CompanyNames = "hatch" | "jarden" | "hortplus";
+type CompanyName = "hatch" | "jarden" | "hortplus";
 
 interface CompanyInfo {
-  applicationName: string;
+  name: string;
   description: string;
   location: string;
   href: string;
-  logo: React.JSX.Element;
   image: StaticImageData;
+  bgClass: string;
+  underlineClass: string;
+  Logo: React.ComponentType<{ className?: string }>;
 }
 
-// Define company information
-const companies: Record<CompanyNames, CompanyInfo> = {
+const companies: Record<CompanyName, CompanyInfo> = {
   hatch: {
-    applicationName: "Hatch Invest",
+    name: "Hatch Invest",
     description: "Jarden & FNZ Joint Venture",
     location: "Wellington, NZ",
     href: "https://www.hatchinvest.nz/",
-    logo: <HatchLogo className="h-5 w-5" />,
-    image: hatch
+    image: hatch,
+    bgClass: "bg-hatch-purple",
+    underlineClass: "after:bg-hatch-purple",
+    Logo: HatchLogo
   },
   jarden: {
-    applicationName: "Jarden Direct",
+    name: "Jarden Direct",
     description: "Jarden",
     location: "Wellington, NZ",
     href: "https://www.jardendirect.co.nz/",
-    logo: <JardenLogo className="h-5 w-5" />,
-    image: jarden
+    image: jarden,
+    bgClass: "bg-direct-green",
+    underlineClass: "after:bg-direct-green",
+    Logo: JardenLogo
   },
   hortplus: {
-    applicationName: "SprayPlan Manager",
+    name: "SprayPlan Manager",
     description: "HortPlus",
     location: "Hawkes Bay, NZ (Remote)",
     href: "https://www.hortplus.com/spray-plan-manager/",
-    logo: <HortplusLogo className="h-5 w-5" />,
-    image: hortplus
+    image: hortplus,
+    bgClass: "bg-hortplus-blue",
+    underlineClass: "after:bg-hortplus-blue",
+    Logo: HortplusLogo
   }
 };
 
-const underlineVariants = cva(
-  "ml-[-0.25rem] w-fit slide-in-underline flex items-center space-x-1.5",
-  {
-    variants: {
-      variant: {
-        default: "after:bg-slate-50",
-        hatch: "after:bg-hatch-purple",
-        jarden: "after:bg-direct-green",
-        hortplus: "after:bg-hortplus-blue"
-      }
-    }
-  }
-);
-
-const backgroundVariants = cva("w-full sm:h-56 bg-gray-300 drop-shadow-md", {
-  variants: {
-    variant: {
-      default: "bg-slate-50",
-      hatch: "bg-hatch-purple",
-      jarden: "bg-direct-green",
-      hortplus: "bg-hortplus-blue"
-    }
-  }
-});
-
-const ExperienceCard = ({ companyName }: { companyName: CompanyNames }) => {
-  const { applicationName, description, location, href, logo, image } = companies[companyName];
+const ExperienceCard = ({ companyName }: { companyName: CompanyName }) => {
+  const { name, description, location, href, image, bgClass, underlineClass, Logo } =
+    companies[companyName];
 
   return (
     <section>
-      {/* Image */}
-      <WobbleCard href={href} containerClassName={backgroundVariants({ variant: companyName })}>
+      <WobbleCard href={href} containerClassName={`w-full sm:h-56 drop-shadow-md ${bgClass}`}>
         <Image
           className="absolute right-[-2rem] top-10 rounded-lg object-contain"
           src={image}
@@ -96,21 +77,20 @@ const ExperienceCard = ({ companyName }: { companyName: CompanyNames }) => {
       </WobbleCard>
 
       <div className="mx-2 mt-2 flex flex-col space-y-1.5">
-        {/* Company and location */}
-        <div className="flex flex-col">
-          <span className="text-sm font-medium leading-tight tracking-tight text-gray-800">
+        <div>
+          <p className="text-sm font-medium leading-tight tracking-tight text-gray-800">
             {description}
-          </span>
-          <span className="text-xs tracking-tight text-slate-500">{location}</span>
+          </p>
+          <p className="text-xs tracking-tight text-slate-500">{location}</p>
         </div>
 
-        {/* Product header with logo */}
         <ExternalLink href={href}>
-          <div className={underlineVariants({ variant: companyName })}>
-            {logo}
-
+          <div
+            className={`slide-in-underline ml-[-0.25rem] flex w-fit items-center space-x-1.5 ${underlineClass}`}
+          >
+            <Logo className="h-5 w-5" />
             <h6 className="text-lg font-semibold tracking-tighter">
-              <span>{applicationName}</span>
+              {name}
               <CircleArrowRight className="mb-0.5 inline-flex h-[1em] pl-1" strokeWidth="2.5px" />
             </h6>
           </div>
